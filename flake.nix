b571641a59376d89a2b72d3a59e69052a3ece575
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2025-09-19 16:19:13 +0800

    finish tutorial01

--- flake.nix
+++ flake.nix
@@ -3,24 +3,36 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          name = "leptjson";
-          packages = with pkgs;
-            [ cmake gnumake ]
-            ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
-          shellHook = ''
-            exec fish
-          '';
-        };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            name = "leptjson";
+            packages =
+              with pkgs;
+              [
+                cmake
+                gnumake
+              ]
+              ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+            shellHook = ''
+              exec fish
+            '';
+          };
+        }
+      );
     };
 }
